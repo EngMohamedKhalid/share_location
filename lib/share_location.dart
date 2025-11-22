a73b3899627f5c01d2cc4f4904a8d3333362233c
Mohamed Khalid <EngMohamedKhalid4404@gmail.com>
2025-11-22 12:36:59 +0200

    handel share

--- lib/share_location.dart
+++ lib/share_location.dart
@@ -1,41 +1,57 @@
 import 'package:geolocator/geolocator.dart';
 import 'package:share_plus/share_plus.dart';
-
 import 'custom_toast.dart';
+import 'package:flutter/widgets.dart';
 
 class ShareMyLocation {
-  static Future<void> shareCurrentLocation() async {
+  static Future<void> shareCurrentLocation({String? shareMessage}) async {
+    final bool isArabic = WidgetsBinding.instance.platformDispatcher.locale.languageCode == "ar";
     bool serviceEnabled;
     LocationPermission permission;
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       await Geolocator.openLocationSettings();
-      CustomToast.error("Location services are disabled.");
-      throw Exception("Location services are disabled.");
+      CustomToast.error(
+          isArabic
+              ? "خدمات تحديد الموقع مغلقة"
+              : "Location services are disabled.",
+      );
+      return;
     }
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        CustomToast.error("Location permission denied.");
-        permission = await Geolocator.requestPermission();
+        CustomToast.error(
+          isArabic
+            ? "تم رفض إذن الموقع"
+            : "Location permission denied.",
+        );
+        return;
       }
     }
+
     if (permission == LocationPermission.deniedForever) {
-      CustomToast.error("Location permission permanently denied.");
-      permission = await Geolocator.requestPermission();
+      CustomToast.error(isArabic
+          ? "تم رفض إذن الموقع بشكل نهائي"
+          : "Location permission permanently denied.",
+      );
+      return;
     }
     Position position = await Geolocator.getCurrentPosition(
       locationSettings: LocationSettings(
         accuracy: LocationAccuracy.high,
       ),
     );
-
     final url =
         "https://www.google.com/maps/search/?api=1&query=${position.latitude},${position.longitude}";
-    await SharePlus.instance.share(ShareParams(
-      subject: "Share My Location",
-      text: url,
-    ));
+    await SharePlus.instance.share(
+      ShareParams(
+        subject: isArabic
+            ? (shareMessage ?? "مشاركة موقعي")
+            : (shareMessage ?? "Share My Location"),
+        text: url,
+      ),
+    );
   }
 }
